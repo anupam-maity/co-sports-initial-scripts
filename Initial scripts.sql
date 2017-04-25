e1276050bde4ccf2e9469619cb865b1e446e6b19
anupam-maity <anupam.maity@harman.com>
2017-04-25 14:17:09 +0530

    Update Initial scripts.sql

--- Initial scripts.sql
+++ Initial scripts.sql
@@ -26,14 +26,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Snooker', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,10);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Squash', 'ECO-SPACE', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Bowling', 'MANYATA', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Table Tennis', 'WHITE-FIELD', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Badminton', 'WHITE-FIELD', STR_TO_DATE('25-04-2017', '%d-%m-%Y'),30,25);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Chess', 'ECO-SPACE', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Volleyball', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Boxing', 'MANYATA', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,5);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Swimming', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Basket ball', 'ECO-SPACE', STR_TO_DATE('25-04-2017', '%d-%m-%Y'),30,12);
-INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_perticipants) VALUES('Carrom', 'ECO-SPACE', STR_TO_DATE('21-04-2017', '%d-%m-%Y'),30,24);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Snooker', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,10);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Squash', 'ECO-SPACE', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Bowling', 'MANYATA', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Table Tennis', 'WHITE-FIELD', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Badminton', 'WHITE-FIELD', STR_TO_DATE('25-04-2017', '%d-%m-%Y'),30,25);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Chess', 'ECO-SPACE', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,20);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Volleyball', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Boxing', 'MANYATA', STR_TO_DATE('23-04-2017', '%d-%m-%Y'),30,5);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Swimming', 'WHITE-FIELD', STR_TO_DATE('22-04-2017', '%d-%m-%Y'),30,30);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Basket ball', 'ECO-SPACE', STR_TO_DATE('25-04-2017', '%d-%m-%Y'),30,12);
+INSERT INTO EVENT(event_name, event_venue, event_date, max_participants,number_of_participants) VALUES('Carrom', 'ECO-SPACE', STR_TO_DATE('21-04-2017', '%d-%m-%Y'),30,24);
